b498f16faee476da49b660daadd8ebaf090cc674
zhhjemotion <zhhjemotoin@hotmail.com>
2016-11-18 22:16:57 +0100

    SURF feature detection

---
+++ SURFfeatureDetection/SURFfeatureDetection/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SURFfeatureDetection --- SURF特征检测
+//
+//  Created by ZHHJemotion on 2016/11/18.
+//  Copyright © 2016年 Lukas_Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
