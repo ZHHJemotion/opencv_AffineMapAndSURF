cfaad923d182e558a6451f5437b0f687416a36d4
zhhjemotion <zhhjemotoin@hotmail.com>
2016-11-19 19:58:57 +0100

    SURFfeatureDetection --- SURF特征检测

--- SURFfeatureDetection/SURFfeatureDetection/main.cpp
+++ SURFfeatureDetection/SURFfeatureDetection/main.cpp
@@ -6,10 +6,23 @@
 //  Copyright © 2016年 Lukas_Zhang. All rights reserved.
 //
 
+#include "opencv2/core/core.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <opencv2/nonfree/nonfree.hpp>
+#include<opencv2/legacy/legacy.hpp>
 #include <iostream>
+
+#define PATH string("/Users/zhangxingjian/Desktop/Programming/C++/OpenCV/opencv_AffineMapAndSURF/SURFfeatureDetection/SURFfeatureDetection/")
+
+using namespace cv;
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+    
+    
     return 0;
 }
