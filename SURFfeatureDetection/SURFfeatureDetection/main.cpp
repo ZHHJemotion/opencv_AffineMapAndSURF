1fd7d266acbe6dd0fad14488be6ed0cb8757f7e7
zhhjemotion <zhhjemotoin@hotmail.com>
2016-11-21 22:09:35 +0100

    SURFfeatureDetection --- SURF特征检测 with SurfDescriptorExtractor

--- SURFfeatureDetection/SURFfeatureDetection/main.cpp
+++ SURFfeatureDetection/SURFfeatureDetection/main.cpp
@@ -10,7 +10,7 @@
 #include "opencv2/features2d/features2d.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include <opencv2/nonfree/nonfree.hpp>
-#include<opencv2/legacy/legacy.hpp>
+#include <opencv2/legacy/legacy.hpp>
 #include <iostream>
 
 #define PATH string("/Users/zhangxingjian/Desktop/Programming/C++/OpenCV/opencv_AffineMapAndSURF/SURFfeatureDetection/SURFfeatureDetection/")
@@ -18,11 +18,68 @@
 using namespace cv;
 using namespace std;
 
+//全局函数申明
+static void showHelpText(); //输出帮助文字
+
+// ----------------------- main() function ------------------------------
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     
+    showHelpText();
     
+    //载入图像
+    Mat srcImage1 = imread(PATH+string("1.jpg"),1);
+    Mat srcImage2 = imread(PATH+string("2.jpg"),1);
+    //异常处理
+    if (!srcImage1.data || !srcImage2.data)
+    {
+        printf("Error! We cannot read these two source images!!!\n\n");
+        return false;
+    }
+    
+    //使用SURF算子检测关键点
+    int minHessian = 700; //SURF算法中的hessian阈值
+    SurfFeatureDetector detector(minHessian); //定义一个SurfFeatureDetector(SURF)特征检测类对象
+    std::vector<KeyPoint> keyPoint1, keyPoint2; //Vector模板类，存放任意类型的动态数组
+    
+    //调用detect函数检测出SURF特征关键点，保存在vector容器中
+    detector.detect(srcImage1, keyPoint1);
+    detector.detect(srcImage2, keyPoint2);
+    
+    //计算描述符（特征向量）
+    SurfDescriptorExtractor extractor;
+    Mat descriptors1, descriptors2;
+    extractor.compute(srcImage1, keyPoint1, descriptors1);
+    extractor.compute(srcImage2, keyPoint2, descriptors2);
+    
+    //使用Bruteforce进行匹配
+    //实例化一个匹配器
+    BruteForceMatcher<L2<float>> matcher;
+    std::vector<DMatch> matches;
+    //匹配两幅图中的描述子（descriptors）
+    matcher.match(descriptors1, descriptors2, matches);
+    
+    //绘制从两个图像中匹配出的特征关键点
+    Mat matchesImage;
+    drawMatches(srcImage1, keyPoint1, srcImage2, keyPoint2, matches, matchesImage);
+    //显示匹配图
+    namedWindow("the match image", WINDOW_AUTOSIZE);
+    imshow("the match image", matchesImage);
+    //保存匹配图
+    imwrite(PATH+string("matchesImage.jpg"), matchesImage);
+    
+    waitKey(0);
     
     return 0;
+}
+
+
+// ------------------------ showHelpText() function -------------------------
+static void showHelpText()
+{
+    //输出一些帮助信息
+    printf("\n\n\n\t欢迎来到【SURF特征描述】示例程序~\n\n");
+    std::cout<<"\t当前使用的OpenCV版本为 OpenCV"<<CV_VERSION;
+    printf("\n\n\t\t\t\t\t\t\t\t by ZHHJemotion\n\n\n");
 }
